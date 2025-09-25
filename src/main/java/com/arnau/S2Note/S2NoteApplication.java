dc1f6f10c2393adc1bd774ef68d9d33a4857dd50
arnaudbecker09 <arnaudtechno9@gmail.com>
2025-09-25 14:27:17 +0200

    feat: init Spring project

---
+++ src/main/java/com/arnau/S2Note/S2NoteApplication.java
@@ -0,0 +1,13 @@
+package com.arnau.S2Note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2NoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2NoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arnau/S2Note/S2NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnau.S2Note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2NoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
