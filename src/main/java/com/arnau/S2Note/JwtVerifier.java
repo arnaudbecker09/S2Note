e4ede99b48dde4a81ae60f53fba628c7eff4ac0c
arnaudbecker09 <arnaudtechno9@gmail.com>
2025-11-14 16:14:07 +0100

    feat: implement JWT verifier

---
+++ src/main/java/com/arnau/S2Note/JwtVerifier.java
@@ -0,0 +1,50 @@
+package com.arnau.S2Note;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtVerifier {
+    private final String SECRET_KEY;
+
+    public JwtVerifier() {
+        this.SECRET_KEY = "a-string-secret-at-least-256-bits-long";
+    }
+
+    public JwtVerifier(String token) {
+        this.SECRET_KEY = token;
+    }
+
+    public String generateJwt(String user) {
+        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+        return Jwts.builder()
+                .setSubject(user)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public void verifyJWT(String token) {
+        try {
+            Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/test/java/com/arnau/S2Note/JwtVerifierTest.java
@@ -0,0 +1,43 @@
+package com.arnau.S2Note;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtVerifierTest {
+
+    private JwtVerifier jwtVerifier;
+
+    @BeforeEach
+    void setup()
+    {
+        jwtVerifier = new JwtVerifier();
+    }
+
+    @Test
+    void shouldValidateJwt() {
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                + "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0."
+                + "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+        assertDoesNotThrow(() -> jwtVerifier.verifyJWT(token));
+
+    }
+
+    @Test
+    void shouldGenerateAndValidateJwt() {
+        String token = jwtVerifier.generateJwt("John Smith");
+        assertNotNull(token);
+        assertTrue(token.startsWith("eyJ"));
+        assertDoesNotThrow(() -> jwtVerifier.verifyJWT(token));
+
+    }
+
+    @Test
+    void shouldFailWithDifferentKey() {
+        JwtVerifier jwtVerifier2 = new JwtVerifier("a-string-secret-at-least-256-bits-long-but-wrong");
+        String token = jwtVerifier.generateJwt("John Smith");
+        assertThrows(RuntimeException.class, () -> jwtVerifier2.verifyJWT(token));
+    }
+
+}
